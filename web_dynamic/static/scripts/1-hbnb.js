cbfdb0b7ac09f9a3668b9c73c6c21d5ac39e06ad
Alemi Herbert <alemiherbert@gmail.com>
2024-06-14 04:00:29 +0300

    task 2 done

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+    let selectedAmenities = {};
+
+    // Listen for changes on each input checkbox
+    $('input[type="checkbox"]').change(function() {
+        const amenityName = $(this).closest('li').text().trim();
+
+        if ($(this).is(':checked')) {
+            selectedAmenities[amenityName] = amenityName;
+        } else {
+            delete selectedAmenities[amenityName];
+        }
+
+        // Update the h4 tag inside the div Amenities
+        let amenitiesList = Object.values(selectedAmenities).join(', ');
+        $('div.amenities h4').text(amenitiesList || 'Amenities');
+    });
+});
