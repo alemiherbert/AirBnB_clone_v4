1b8cc2bb3d9d426438267027616fcb1e1986874b
Solotole <solomontoleak47@gmail.com>
2024-06-15 06:50:14 +0100

    task 2

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+    let selectedAmenities = {};
+
+    // Listen for changes on each input checkbox
+    $('input[type="checkbox"]').change(function() {
+        const amenityName = $(this).closest('li').text().trim();
+
+        if ($(this).is(':checked')) {
+            selectedAmenities[amenityName] = amenityName;
+        } else {
+            delete selectedAmenities[amenityName];
+        }
+
+        // Update the h4 tag inside the div Amenities
+        let amenitiesList = Object.values(selectedAmenities).join(', ');
+        $('div.amenities h4').text(amenitiesList || 'Amenities');
+    });
+    // sending a request
+    $.get("http://0.0.0.0:5001/api/v1/status/", function(data) {
+	if (data.status === "OK") {
+	   $('div#api_status').addClass("available");
+	} else {
+	   $('div#api_status').removeClass("available");
+	}
+    });
+});
